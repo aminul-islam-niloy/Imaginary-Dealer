7ff58ca6a31a6670980b339c36406beae61a74fb
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-08-23 18:36:11 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Imaginary_Dealer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
