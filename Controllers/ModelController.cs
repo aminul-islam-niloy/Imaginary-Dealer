8f6bb64a32e66f4533d16a4c019bc02cc07af80c
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-08-25 22:44:03 +0600

    Emplement CRUD and Add bootstrap

---
+++ Controllers/ModelController.cs
@@ -0,0 +1,36 @@
+﻿using Imaginary_Dealer.AppDBContex;
+using Imaginary_Dealer.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Imaginary_Dealer.Controllers
+{
+    public class ModelController : Controller
+    {
+        public readonly Im_Dealer_DB_Contex _db;
+       
+        [BindProperty]
+      public ModelViewModel ViewModel { get; set; }
+
+        public ModelController(Im_Dealer_DB_Contex db)
+        {
+            _db = db;
+            ViewModel = new ModelViewModel()
+            {
+                Brands = _db.Brands.ToList(),
+                Model = new Models.Model()
+
+            };
+        }
+
+        public IActionResult Index()
+        {
+            var model = _db.Models.Include(m => m.Brand);
+            return View(model);
+        }
+
+        public IActionResult Create() { 
+        return View(ViewModel);
+        }
+    }
+}
---
+++ Models/ViewModels/ModelViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Imaginary_Dealer.Models.ViewModels
+{
+    public class ModelViewModel
+    {
+        public Model Model { get; set; }
+        public IEnumerable<Brand> Brands { get; set; }
+
+        public IEnumerable<SelectListItem> CSelectListItem(IEnumerable<Brand> Items)
+        {
+            List<SelectListItem> Makelist = new List<SelectListItem>();
+            SelectListItem item = new SelectListItem
+            {
+                Text = "---Select---",
+                Value = "0"
+            };
+            Makelist.Add(item);
+
+            foreach (Brand brand in Items)
+            {
+                item = new SelectListItem
+                {
+                    Text = brand.Name,
+                    Value = brand.Id.ToString()
+                };
+                Makelist.Add(item);
+            }
+            return Makelist;
+        }
+
+    }
+}
